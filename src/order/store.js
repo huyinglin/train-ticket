ac9300a42ece7fcb634edf3fe24475a14855aaea
huyinglin <fazheng@indata.cc>
2020-01-30 11:36:46 +0800

    feat: init project

---
+++ src/order/store.js
@@ -0,0 +1,14 @@
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers,
+} from 'redux';
+
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+export default createStore(
+  combineReducers(reducers),
+  {},
+  applyMiddleware(thunk),
+);
---
+++ src/query/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+
+function App(props) {
+  return (
+    <div>
+      dsg
+    </div>
+  );
+}
+
+const mapStateToProps = (state, props) => {};
+
+const mapDispatchToProps = (dispatch) => {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
